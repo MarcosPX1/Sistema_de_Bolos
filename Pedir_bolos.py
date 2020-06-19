3dbf20ad073c68d35241041721100d91a8c32f2d
Marcos <37980760+MarcosPX1@users.noreply.github.com>
2020-06-19 12:19:52 -0300

    Alterações importantes
    Feitas alterações na função Caixa para que o valor seja retornado corretamente no final da conta.
    Feitas alterações também na função de  selecionar o sabor do bolo, para que o usuário final não seja obrigado a digitar o nome do sabor, apenas um número da sequência dos sabores na lista . EX : Chocolate , Limão (Ao solicitar para o usuário o mesmo indicará 1 para chocolate , 2 para limão e etc.).

--- Pedir_bolos.py
+++ Pedir_bolos.py
@@ -26,17 +26,40 @@ def escolher_menu():
 def seleciona_sabor_bolo():
     print("********* Sabores disponíveis abaixo ********* ")
     print(sabor_bolo, '\n')
+    print("Utilize os números de 1 a 5 para selecionar os sabores na ordem")
     bolo = input("Favor indicar o sabor do bolo: ")
+    if bolo == '1':
+        bolo = sabor_bolo[0]
+    elif bolo == '2':
+        bolo = sabor_bolo[1]
+    elif bolo == '3':
+        bolo = sabor_bolo[2]
+    elif bolo == '4':
+        bolo = sabor_bolo[3]
+    elif bolo == '5':
+        bolo = sabor_bolo[4]
+
     # Se o sabor for algum da var sabor_bolo deverá ser armazenado este produto em produtos_selecionados[]
     if bolo in sabor_bolo:  # Se for selecionado alguma string tal qual as que se encontram na lista, será feito a inclusão na lista de produtos selecionados
         produtos_selecionados_bolo.append(bolo)
     while bolo in sabor_bolo:  # Enquanto bolo estiver dentro dos valores da Var sabor bolo faça
-        opcao = input('Deseja adicionar mais algum sabor? : ')  # Armazena uma nova opção de sabor digitada pelo usuário
-        if opcao == 'Sim':
+        opcao = int(input(
+            'Deseja adicionar mais algum sabor? (1 para Sim e 2 para Não) : '))  # Armazena uma nova opção de sabor digitada pelo usuário
+        if opcao == 1:
             bolo = input("Favor indicar o sabor do bolo: ")
+            if bolo == '1':
+                bolo = sabor_bolo[0]
+            elif bolo == '2':
+                bolo = sabor_bolo[1]
+            elif bolo == '3':
+                bolo = sabor_bolo[2]
+            elif bolo == '4':
+                bolo = sabor_bolo[3]
+            elif bolo == '5':
+                bolo = sabor_bolo[4]
             if bolo in sabor_bolo:
                 produtos_selecionados_bolo.append(bolo)
-        elif opcao == 'Não':
+        elif opcao == 2:
             escolher_menu()
             break
 
@@ -49,12 +72,13 @@ def seleciona_sabor_torta():
     if torta in sabor_tortas:  # Se for selecionado alguma string tal qual as que se encontram na lista, será feito a inclusão na lista de produtos selecionados
         produtos_selecionados_torta.append(torta)
     while torta in sabor_tortas:  # Enquanto torta estiver dentro da lista da var sabor tortas faça:
-        opcao = input('Deseja adicionar mais algum sabor? : ')  # Armazena uma nova opção de sabor digitada pelo usuário
-        if opcao == 'Sim':
+        opcao = input(
+            'Deseja adicionar mais algum sabor? (1 para Sim e 2 para Não) : ')  # Armazena uma nova opção de sabor digitada pelo usuário
+        if opcao == 1:
             torta = input("Favor indicar o sabor da torta: ")
             if torta in sabor_tortas:
                 produtos_selecionados_torta.append(torta)
-        elif opcao == 'Não':
+        elif opcao == 1:
             escolher_menu()
             break
 
@@ -74,11 +98,20 @@ def olhar_itens():
 
 def caixa():
     valor_total = 0
-     # chocolate - 2 , limão - 2 , morango - 4 , prestigio - 3 , nutella - 5
-    if produtos_selecionados_bolo[0] == sabor_bolo[0]:
+    # chocolate - 2 , limão - 2 , morango - 4 , prestigio - 3 , nutella - 5
+    # Se os valores de SaborBolo estiverem nas posições 0 a 4 da variavel produtos_selecionados_bolo - Some o valor total com o valor do sabor
+
+    if sabor_bolo[0] in produtos_selecionados_bolo[0:4]:
         valor_total = valor_total + 2
-    elif produtos_selecionados_bolo[1] == sabor_bolo[1]:
+    if sabor_bolo[1] in produtos_selecionados_bolo[0:4]:
         valor_total = valor_total + 2
+    if sabor_bolo[2] in produtos_selecionados_bolo[0:4]:
+        valor_total = valor_total + 4
+    if sabor_bolo[3] in produtos_selecionados_bolo[0:4]:
+        valor_total = valor_total + 3
+    if sabor_bolo[4] in produtos_selecionados_bolo[0:4]:
+        valor_total = valor_total + 5
+
     print("Sua conta deu exatamente :", valor_total)
 
 
